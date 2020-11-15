e50570ddfba3f4edb7315b8586bf1821f83cf8bf
nour.trab123 <nour.trab23@gmail.com>
2020-11-15 09:16:38 +0100

    Composant listmateriel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcceuilsComponent } from './composants/acceuils/acceuils.component';
+import { ListmaterielComponent } from './composants/informatiques/listmateriel/listmateriel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcceuilsComponent
+    AcceuilsComponent,
+    ListmaterielComponent
   ],
   imports: [
     BrowserModule,
